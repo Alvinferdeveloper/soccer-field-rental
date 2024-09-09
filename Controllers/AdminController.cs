cc1483b77546cfacf7fef9b6dfd0d06f1359ccfd
Albin <alvinfer67@gmail.com>
2024-09-08 19:06:26 -0600

    Added: Admin Dashboard functionality

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -12,7 +12,7 @@ namespace proyectoCanchas.Controllers;
 [Authorize(Roles = "ADMIN")]
 public class AdminController : Controller
 {
-     private readonly AppDbContext _context;
+    private readonly AppDbContext _context;
 
     public AdminController(AppDbContext context)
     {
@@ -25,23 +25,34 @@ public class AdminController : Controller
         return View();
     }
 
-    public IActionResult Dashboard(){
+    public IActionResult Dashboard()
+    {
         var rentsCampo1 = _context.Rents.Where(r => r.FieldId == 1 && r.Active == true).Include(r => r.Field);
-        var rentsCampo2 = _context.Rents.Where(r => r.FieldId ==2 && r.Active == true).Include(r => r.Field);
-        var totalGananciaCampo1 = rentsCampo1.Any() 
-    ? rentsCampo1.FirstOrDefault()?.Field.Price * rentsCampo1.Count() 
+        var rentsCampo2 = _context.Rents.Where(r => r.FieldId == 2 && r.Active == true).Include(r => r.Field);
+        var totalGananciaCampo1 = rentsCampo1.Any()
+    ? rentsCampo1.FirstOrDefault()?.Field.Price * rentsCampo1.Count()
     : 0;
-    var totalGananciaCampo2 = rentsCampo2.Any() 
-    ? rentsCampo2.FirstOrDefault()?.Field.Price * rentsCampo2.Count() 
-    : 0;
+        var totalGananciaCampo2 = rentsCampo2.Any()
+        ? rentsCampo2.FirstOrDefault()?.Field.Price * rentsCampo2.Count()
+        : 0;
 
-    ViewBag.totalGananciaCampo1 = totalGananciaCampo1;
-    ViewBag.totalGananciaCampo2 = totalGananciaCampo2;
+        DateTime startOfMonth = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+        DateTime today = DateTime.Now;
+        var gananciaCampo1EnMes = rentsCampo1.Any() ? rentsCampo1.FirstOrDefault()?.Field.Price *  rentsCampo1.Where(r => r.DateTime >= startOfMonth && r.DateTime <= today).Count() : 0;
+        var gananciaCampo2EnMes = rentsCampo2.Any() ? rentsCampo2.FirstOrDefault()?.Field.Price *  rentsCampo2.Where(r => r.DateTime >= startOfMonth && r.DateTime <= today).Count() : 0;
     
-        
+
+        ViewBag.totalGananciaCampo1 = totalGananciaCampo1;
+        ViewBag.totalGananciaCampo2 = totalGananciaCampo2;
+        ViewBag.totalRentasCampo1 = rentsCampo1.Count();
+        ViewBag.totalRentasCampo2 = rentsCampo2.Count();
+        ViewBag.gananciaCampo1EnMes = gananciaCampo1EnMes;
+        ViewBag.gananciaCampo2EnMes = gananciaCampo2EnMes;
+
+
         return View();
     }
-   
 
-    
+
+
 }
